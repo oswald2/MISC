0ed3bb33bb0cb522331ab6fbd7999f11faa8466b
Michael Oswald <michael.oswald@atos.net>
2018-05-03 19:07:05 +0200

    got rid of warnings

--- src/SCOS/MISC/MISCConfigParser.hs
+++ src/SCOS/MISC/MISCConfigParser.hs
@@ -2,12 +2,14 @@
 module SCOS.MISC.MISCConfigParser
   ( MISCconfig
   , DynamicVar(..)
+  , StaticVar(..)
   , parseConfigFile
   , getResourceStatic
   , getResourceDynamic
   )
 where
 
+import Control.Monad (void)
 
 import           Data.Text.Lazy                 ( Text )
 import qualified Data.Text.Lazy                as T
@@ -53,29 +55,20 @@ parseConfigFile path = do
         -> Either Text MISCconfig
     go _ [] stat dyn = Right (MISCconfig stat dyn)
     go False (line : ls) stat dyn
-        | T.strip line == ""
-        = go False ls stat dyn
-        | line `T.index` 0 == '#'
-        = go False ls stat dyn
-        | T.strip line == "DYNAMIC"
-        = go True ls stat dyn
-        | otherwise
-        = case parse staticVariable "" (T.toStrict line) of
+        | T.strip line == "" = go False ls stat dyn
+        | line `T.index` 0 == '#' = go False ls stat dyn
+        | T.strip line == "DYNAMIC" = go True ls stat dyn
+        | otherwise = case parse staticVariable "" (T.toStrict line) of
             Left err -> Left (T.pack (show err))
-            Right new -> 
-                let hm = HM.insert (statVar new) new stat
-                in  go False ls hm dyn
+            Right new ->
+                let hm = HM.insert (statVar new) new stat in go False ls hm dyn
     go True (line : ls) stat dyn
-        | T.strip line == ""
-        = go True ls stat dyn
-        | line `T.index` 0 == '#'
-        = go True ls stat dyn
-        | otherwise
-        = case parse dynamicVariable "" (T.toStrict line) of
+        | T.strip line == "" = go True ls stat dyn
+        | line `T.index` 0 == '#' = go True ls stat dyn
+        | otherwise = case parse dynamicVariable "" (T.toStrict line) of
             Left err -> Left (T.pack (show err))
-            Right new -> 
-                let hm = HM.insert (dynVar new) new dyn
-                in  go True ls stat hm
+            Right new ->
+                let hm = HM.insert (dynVar new) new dyn in go True ls stat hm
 
 
 getResourceStatic :: MISCconfig -> Text -> Maybe StaticVar
@@ -86,22 +79,23 @@ getResourceDynamic conf name = HM.lookup name (miscDynamic conf)
 
 
 
-
+staticVariable :: Parser StaticVar
 staticVariable = do
     i <- identifier
     v <- try (endOfLine >> return T.empty) <|> do
         spaces
         v <- value
-        endOfLine
+        void $ endOfLine
         return v
     pure (StaticVar i v)
 
+dynamicVariable :: Parser DynamicVar
 dynamicVariable = do
     i <- identifier
     spaces
     p <- oneOf "NY"
     spaces
-    m <- oneOf "NY"
+    m          <- oneOf "NY"
     (v, param) <- try (endOfLine >> return (T.empty, Nothing)) <|> parseValue m
     pure (DynamicVar i p m param v)
   where
@@ -112,12 +106,12 @@ dynamicVariable = do
         d <- identifier
         spaces
         v <- value
-        endOfLine
+        void $ endOfLine
         return (v, Just d)
     valueOnly = do
         spaces
         v <- value
-        endOfLine
+        void $ endOfLine
         return (v, Nothing)
 
 identifier :: Parser Text
@@ -129,12 +123,14 @@ value = T.strip . T.pack <$> many (oneOf valBaseChars)
 
 
 
-
+baseChars :: [Char]
 baseChars =
     ['a' .. 'z']
         ++ ['A' .. 'Z']
         ++ ['0' .. '9']
         ++ "_-$%^&*()~`':;\"|/?<>[]{}.,"
+
+valBaseChars :: [Char]
 valBaseChars =
     ['a' .. 'z']
         ++ ['A' .. 'Z']
