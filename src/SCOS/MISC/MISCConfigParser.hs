4de1415f8a362b84cdf913b63253c97732edc384
Michael Oswald <michael.oswald@atos.net>
2018-05-03 18:08:54 +0200

    Updates to get full values for dynamic variables

--- src/SCOS/MISC/MISCConfigParser.hs
+++ src/SCOS/MISC/MISCConfigParser.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 module SCOS.MISC.MISCConfigParser
   ( MISCconfig
-  , DynVar(..)
+  , DynamicVar(..)
   , parseConfigFile
   , getResourceStatic
   , getResourceDynamic
@@ -12,21 +12,25 @@ where
 import           Data.Text.Lazy                 ( Text )
 import qualified Data.Text.Lazy                as T
 import qualified Data.Text.Lazy.IO             as T
+import           Text.Parsec
 
-import Data.HashMap.Strict (HashMap)
-import qualified Data.HashMap.Strict as HM 
+import           Data.HashMap.Strict (HashMap)
+import qualified Data.HashMap.Strict as HM
 
 
-data DynVar = DynVar {
-    dynName :: !Text,
-    dynValue :: !Text
+data DynamicVar = DynamicVar {
+    dynVar :: T.Text,
+    dynPersistent :: Char,
+    dynMirror :: Char,
+    dynParam :: Maybe T.Text,
+    dynValue :: T.Text
+    } deriving Show
+
+
+data MISCconfig = MISCconfig {
+    miscStatic :: HashMap Text Text,
+    miscDynamic :: HashMap Text DynamicVar
 }
-
-data MISCconfig = MISCconfig
-    {
-        miscStatic :: HashMap Text Text,
-        miscDynamic :: HashMap Text DynVar
-    }
 
 
 parseConfigFile :: FilePath -> IO MISCconfig
@@ -34,26 +38,34 @@ parseConfigFile path = do
     content <- T.readFile path
     let cont = T.lines content
     return (go False cont HM.empty HM.empty)
-    where 
-        go :: Bool -> [Text] -> HashMap Text Text -> HashMap Text DynVar -> MISCconfig
-        go _ [] stat dyn = MISCconfig stat dyn
-        go False (line:ls) stat dyn  
-            | T.strip line == "" = go False ls stat dyn
-            | line `T.index` 0 == '#' = go False ls stat dyn
-            | T.strip line == "DYNAMIC" = go True ls stat dyn
-            | otherwise = 
-                let [name, value] = T.words line 
-                    newStat = HM.insert name value stat
-                in  
-                go True ls newStat dyn
-        go True (line:ls) stat dyn 
-            | T.strip line == "" = go True ls stat dyn
-            | line `T.index` 0 == '#' = go True ls stat dyn
-            | otherwise = 
-                let [name, _permanent, _packet, value] = T.words line            
-                    newDyn = HM.insert name (DynVar name value) dyn
-                in 
-                go True ls stat newDyn
+  where
+    go
+        :: Bool
+        -> [Text]
+        -> HashMap Text Text
+        -> HashMap Text DynVar
+        -> MISCconfig
+    go _ [] stat dyn = MISCconfig stat dyn
+    go False (line : ls) stat dyn
+        | T.strip line == ""
+        = go False ls stat dyn
+        | line `T.index` 0 == '#'
+        = go False ls stat dyn
+        | T.strip line == "DYNAMIC"
+        = go True ls stat dyn
+        | otherwise
+        = let [name, value] = T.words line
+              newStat       = HM.insert name value stat
+          in  go True ls newStat dyn
+    go True (line : ls) stat dyn
+        | T.strip line == ""
+        = go True ls stat dyn
+        | line `T.index` 0 == '#'
+        = go True ls stat dyn
+        | otherwise
+        = let [name, _permanent, _packet, value] = T.words line
+              newDyn = HM.insert name (DynVar name value) dyn
+          in  go True ls stat newDyn
 
 
 
@@ -62,3 +74,61 @@ getResourceStatic conf name = HM.lookup name (miscStatic conf)
 
 getResourceDynamic :: MISCconfig -> Text -> Maybe DynVar
 getResourceDynamic conf name = HM.lookup name (miscDynamic conf)
+
+
+
+
+staticVariable = do
+    i <- identifier
+    v <- try (endOfLine >> return T.empty) <|> do
+        spaces
+        v <- value
+        endOfLine
+        return v
+    return (Just (StaticVar i v))
+
+dynamicVariable = do
+    i <- identifier
+    spaces
+    p <- oneOf "NY"
+    spaces
+    m          <- oneOf "NY"
+
+    (v, param) <- try (endOfLine >> return (T.empty, Nothing)) <|> parseValue m
+    return (Just (DynamicVar i p m param v))
+  where
+    parseValue 'Y' = withParam
+    parseValue _   = valueOnly
+    withParam = do
+        spaces
+        d <- identifier
+        spaces
+        v <- value
+        endOfLine
+        return (v, Just d)
+    valueOnly = do
+        spaces
+        v <- value
+        endOfLine
+        return (v, Nothing)
+
+identifier :: Parser T.Text
+identifier = T.pack <$> many1 (oneOf baseChars)
+
+
+value :: Parser T.Text
+value = T.strip . T.pack <$> many (oneOf valBaseChars)
+
+
+
+
+baseChars =
+    ['a' .. 'z']
+        ++ ['A' .. 'Z']
+        ++ ['0' .. '9']
+        ++ "_-$%^&*()~`':;\"|/?<>[]{}.,"
+valBaseChars =
+    ['a' .. 'z']
+        ++ ['A' .. 'Z']
+        ++ ['0' .. '9']
+        ++ " _-=+$%^&*()~`':;\"|/?<>[]{}.,\t"
